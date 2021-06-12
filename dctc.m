6b63dbb9960859f4b3459a83bff1a563e3ecaf84
gumingxuan <75777290+gumingxuan@users.noreply.github.com>
2021-06-12 16:50:12 +0800

    Update dctc.m

--- dctc.m
+++ dctc.m
@@ -1,7 +1,9 @@
+  
+% China University of Petroleum, Beijing
+% Mingxuan Gu
+% gumingxuan_cup@163.com
+
 function [ Ai, bi] = dctc( A ,b, nc )
-%UNTITLED2 此处显示有关此函数的摘要
-             %DCT压缩    返回值是 压缩后的 A b 
-%   此处显示详细说明
 dctA=dct2(A);
 dct_echonoise=dct(b);
 dctA1=dctA(1:nc,:);
